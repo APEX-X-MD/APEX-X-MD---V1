71c6a08a5248471b19632d42a061e0035e7ae2b2
APEX-X-MD <dtech3314@gmail.com>
2024-12-07 19:51:21 +0530

    Update song&video.js

--- plugins/song&video.js
+++ plugins/song&video.js
@@ -34,9 +34,53 @@ await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:m
 let down = await fg.yta(url)
 let downloadUrl = down.dl_url
 
-//send audio message
+//send audio + document message
 await conn.sendMassage(from,{audio: {url:downloadUrl},mimetype:"aduio/mpeg"},{quoted:mek})
+await conn.sendMassage(from,{document: {url:downloadUrl},minetype:"audio/mpeg",fileName:data.title + ".mp3",caption:"> 💝MADE BY SETHIKA"},{quoted:mek})
 
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
+
+//============videodl=============
+
+cmd({
+    pattern: "video",
+    desc: "download videos",
+    category: "download",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply("*Please give me a `URL` or `TITLE` !  *")
+const search = await yts(q)
+const data = search.video[0];
+const url = data.url 
+
+let dec = `
+*_💥 Apex x MD Video Downloader 💥_*
+
+title: ${data.title}
+description: ${data.description}
+time: ${data.timestamp}
+ago: ${data.ago}
+views: ${data.views}
+
+> MADE BY SETHIKA 💝
+`
+await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:mek});
+
+//download video
+
+let down = await fg.ytv(url)
+let downloadUrl = down.dl_url
+
+//send video+ document message
+await conn.sendMassage(from,{video: {url:downloadUrl},mimetype:"video/mp4"},{quoted:mek})
+await conn.sendMassage(from,{document: {url:downloadUrl},minetype:"video/mp4",fileName:data.title + ".mp4",ccaption:"> 💝MADE BY SETHIKA"},{quoted:mek})
 
 
 }catch(e){
