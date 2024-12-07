7680e37eeee37c4c310cb2ea8ee1291146207dc1
APEX-X-MD <dtech3314@gmail.com>
2024-12-07 17:22:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "OYIBlLxI#nWAtmvL6rRhh5TOLJlWROo4UVyqg9VE6C-PwFliTpsw",
-ALIVE_IMG : process.env.ALIVE_IMG || "https://i.ibb.co/tPNQjmX/20241128-184557.jpg",
+ALIVE_IMG : process.env.ALIVE_IMG || "https://i.ibb.co/NL4X84z/Picsart-24-12-03-16-03-40-264.jpg",
 ALIVE_MSG : process.env.ALIVE_MSG || "*_👋Hey There, Im Alive Now☺️.How Can I Help You🙃_*",
 };
